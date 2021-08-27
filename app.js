394bdf64723be9834728bf251909b801ffeb3340
kc <kc.dark@protonmail.com>
2021-08-27 22:57:05 +0530

    add router middleware

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // importing modules
 const express = require('express');
 const db = require('./connection');
+const courseRouter = require('./routes/course');
+const userRouter = require('./routes/user');
+
+// initializing express app
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use('/courses', courseRouter);
+app.use('/user', userRouter);
+
+// listening on port 3000
 app.listen(3000, () => {
     console.log("server is running");
 });
