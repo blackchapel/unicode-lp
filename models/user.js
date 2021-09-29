c7a9a93e43f7947984114e35c34876609c550c4f
kc <kc.dark@protonmail.com>
2021-09-29 21:25:14 +0530

    Improved the schema

--- models/user.js
+++ models/user.js
@@ -5,18 +5,23 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
+        trim: true,
         required: true
     },
     username: {
         type: String,
+        trim: true,
         required: true,
         minlength: [4, 'Username too short'],
         unique: true
     },
     email: {
         type: String,
+        required: true,
+        trim: true,
         lowercase: true,
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+        unique: true
     },
     password: { 
         type: String,
@@ -24,7 +29,7 @@ const userSchema = new mongoose.Schema({
         minlength: [8, 'Password too short'],
         maxlength: [128, 'Password too long']
     },
-    userType: { type: String },
+    userType: { type: [String] },
     birthday: { type: Date },
     enrolledIn: {
         type: [mongoose.Schema.Types.ObjectId],
