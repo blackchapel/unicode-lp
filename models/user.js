7adaa45e32ba2dc23a681cb59de3f5f36aafaaf4
kc <kc.dark@protonmail.com>
2021-09-29 22:57:48 +0530

    Added password hashing and credentials validation

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 // Importing the module
 const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 
 // Creating a schema
 const userSchema = new mongoose.Schema({
@@ -41,6 +42,33 @@ const userSchema = new mongoose.Schema({
         ref: 'Course'
     } 
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+
+    const isMatch = await bcryptjs.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+
+    return user;
+}
+
+// Hashing the password
+userSchema.pre('save',async function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        user.password = await bcryptjs.hash(user.password, 8);
+    }
+
+    next();
+})
 
 const User = mongoose.model('User', userSchema);
 
