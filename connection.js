f906a42a7db2ee807b612ffb6758a2af9a0b51ae
kc <kc.dark@protonmail.com>
2022-01-04 19:37:38 +0530

    created different enviornment for testing

--- connection.js
+++ connection.js
@@ -7,8 +7,17 @@ const connectionParameters = {
     useUnifiedTopology: true
 };
 
+// Creating different enviornments
+let uri;
+
+if (process.env.ENVIRONMENT === 'production') {
+    uri = process.env.URI;
+} else if (process.env.ENVIRONMENT === 'test') {
+    uri = process.env.URI_TEST;
+}
+
 // Connecting to the database
-const connection = mongoose.connect(process.env.MONGODB_URI, connectionParameters)
+const connection = mongoose.connect(uri, connectionParameters)
     .then(() => {
         console.log("Connected to database");
     })
